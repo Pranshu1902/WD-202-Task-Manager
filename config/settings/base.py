ec5c834c5b54dcb32cb644595c432ebba117629c
Pranshu1902 <pranshu1902@gmail.com>
2022-03-01 02:59:22 +0530

    login/logout redirect, email notifier link

--- config/settings/base.py
+++ config/settings/base.py
@@ -108,9 +108,12 @@ AUTHENTICATION_BACKENDS = [
 # https://docs.djangoproject.com/en/dev/ref/settings/#auth-user-model
 AUTH_USER_MODEL = "users.User"
 # https://docs.djangoproject.com/en/dev/ref/settings/#login-redirect-url
-LOGIN_REDIRECT_URL = "users:redirect"
+LOGIN_REDIRECT_URL = "/home/all"
 # https://docs.djangoproject.com/en/dev/ref/settings/#login-url
 LOGIN_URL = "account_login"
+
+# logout redirect
+LOGOUT_REDIRECT_URL = "/user/login"
 
 # PASSWORDS
 # ------------------------------------------------------------------------------
